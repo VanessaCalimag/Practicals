fce096e705f8e9d0142865c8243f4a1f44b52445
Vanessa Calimag <vanessa.calimag@my.jcu.edu.au>
2018-09-21 17:18:28 +1000

    Add function to calculate the conversation from mi to km

--- prac_07/convert_mi_to_km.py
+++ prac_07/convert_mi_to_km.py
@@ -1,11 +1,20 @@
 from kivy.app import App
 from kivy.lang import Builder
-
+from kivy.core.window import Window
 MILES_TO_KILOMETERS = 1.609344
+
 
 class MilesConverterApp(App):
     def build(self):
+        Window.size = (500, 200)
         self.title = "Miles to Kilometers Converter"
         self.root = Builder.load_file('convert_mi_to_km.kv')
         return self.root
+
+    def calculate_mi_to_km(self, value):
+        result = value * MILES_TO_KILOMETERS
+        self.root.ids.output_label.text = str (result)
+
+
+MilesConverterApp().run()
 
