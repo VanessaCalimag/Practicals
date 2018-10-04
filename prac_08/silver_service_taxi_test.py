3f114c2d70133ecdea706ef8d8a87cbb71078060
Vanessa Calimag <vanessa.calimag@my.jcu.edu.au>
2018-10-04 14:08:58 +1000

    Create unreliable car file which checks the car reliability. Create silver service taxi and its test files that were inherited from Taxi file, which adds price to fare due to the car's fanciness.

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,11 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    fancy_taxi = SilverServiceTaxi("Mercedes", 100, 2)
+    fancy_taxi.drive(18)
+    print(fancy_taxi)
+    print(fancy_taxi.get_fare())
+
+
+main()
